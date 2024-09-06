f53b3cccbae6e47daeb71c5e1830b0360c699b91
cientstaaa <cientistaprogramador@gmail.com>
2024-09-06 11:34:37 -0300

    my test site
    for tests...

---
+++ app.js
@@ -0,0 +1,61 @@
+function pesquisar() {
+    // Obtém a seção HTML onde os resultados da pesquisa serão exibidos.
+    let section = document.getElementById
+    ("resultados-pesquisa");
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+    // se campoPesuisa for uma string sem nada...
+    if (!campoPesquisa) {
+      section.innerHTML = "<p> Game nenhum encontrado ;-; </p>"  
+      return 
+   }
+
+   campoPesquisa = campoPesquisa.toLowerCase()
+  
+    // Inicializa uma string vazia para armazenar os resultados formatados.
+    let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags  = "";
+  
+    // Itera sobre cada jogo na lista de jogos.
+    for (let game of games) {
+
+      // deixa tudo minusculo
+      titulo = game.titulo.toLowerCase()
+      descricao = game.descricao.toLowerCase()
+      tags = game.tags.toLowerCase()
+
+      // se titulo includes campoPesquisa
+      if(
+      titulo.includes(campoPesquisa) ||
+      descricao.includes(campoPesquisa) ||
+      tags.includes(campoPesquisa)
+        ) {
+
+         // Constrói o HTML para cada item da pesquisa, formatando o título, descrição e link.
+        resultados += `
+        <div class="item-resultado">
+          <h2>
+            <a href="#" target="_blank">${game.titulo}</a>
+          </h2>
+          <p class="descricao-meta">
+            ${game.descricao}
+          </p>
+          <a href="${game.link}" target="_blank">
+            Veja mais sobre o jogo pesquisado.
+          </a>
+        </div>
+      `;
+      }
+ 
+    }
+
+    if (!resultados) {
+        resultados = " <p> Sem Games adicionados com esse atributo ;-; </p>"
+      }
+  
+    // Atribui o HTML gerado à seção de resultados, substituindo o conteúdo anterior.
+    section.innerHTML = resultados;
+  }
---
+++ games.js
@@ -0,0 +1,38 @@
+let games = [
+    {
+        titulo : "Super Mario Bros",
+        descricao : " super mario bros.: Uma Jornada Clássica Super Mario Bros. é um marco nos jogos eletrônicos, lançado originalmente para o Nintendo Entertainment System (NES).",
+        link :"https://pt.wikipedia.org/wiki/Super_Mario_Bros.",
+        tags : "mario plataforma nes"
+    },
+    {
+        titulo : "Pokemon Fire Red",
+        descricao : "pokémon fire red: Uma aventura inesquecível em Hoenn! Reviva a emocionante jornada de um treinador Pokémon em busca de se tornar um Mestre Pokémon.",
+        link :"https://pt.wikipedia.org/wiki/Pokémon_Fire_Red_e_LeafGreen",
+        tags : "pokemon rpg gba"
+    },
+    {
+        titulo : "Sonic the Hedgehog",
+        descricao : "sonic the Hedgehog: A velocidade a toda! Corra pelos níveis desafiadores do Dr. Eggman e salve o mundo com o ouriço azul mais rápido do mundo.",
+        link : "https://pt.wikipedia.org/wiki/Sonic_the_Hedgehog_(jogo_de_videogame)",
+        tags : "velocidade plataforma mg "
+    },
+    {
+        titulo: "Chrono Trigger",
+        descricao: "chrono Uma aventura épica que transcende o tempo! Junte-se a Crono e seus amigos em uma jornada através de diferentes eras para salvar o mundo de um terrível futuro. Desvende mistérios, lute contra monstros e experimente uma história emocionante.",
+        link: "https://pt.wikipedia.org/wiki/Chrono_Trigger",
+        tags : "crono rpg snes"
+    },
+    {
+        titulo: "Donkey Kong 2: Diddy's Kong Quest",
+        descricao: "donkey kong 2, Ajude Diddy Kong a resgatar seu pai, Donkey Kong, das garras do malvado King K. Rool! Explore diversas fases, colete bananas e enfrente desafiadores inimigos nesta aventura clássica do SNES.",
+        link: "https://www.nintendo.com/games/detail/donkey-kong-2-diddys-kong-quest-switch/",
+        tags : " donkey plataforma snes "
+    },
+    {
+        titulo: "Ms. Pac-Man (SNES)",
+        descricao: "A clássica ms pacman chega ao Super Nintendo! Desfrute de horas de diversão comendo pastilhas e fugindo dos fantasmas em labirintos desafiadores. Uma versão aprimorada do arcade original.",
+        link: "https://lista.mercadolivre.com.br/snes-ms-pacman",
+        tags : " pacman comecome snes"
+    }
+];
